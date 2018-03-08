fdca783e72479bd9c26a9d06a028e7cf1aa6142e
lnormoyle <37177310+lnormoyle@users.noreply.github.com>
2018-03-08 10:17:08 +0000

    Create BinOp.py

---
+++ tests/BinOp.py
@@ -0,0 +1,30 @@
+def LShift(a,b)
+	return a<<b
+
+def RShift(a,b)
+	return a>>b
+
+def BitOr(a,b)
+	return a|b
+
+def BitXor(a,b)
+	return a^b
+
+def BitAnd(a,b)
+	return a&b
+
+def Mod(a,b)
+	return a%b
+
+def Div(a,b)
+	return a/b
+
+def Mult(a,b)
+	return a*b
+
+def Sub(a,b)
+	return a-b
+
+def Add(a,b)
+	return a*b
+
